c40879743a37e6e341779211fd13adec4999aa86
Eva He <appleheshuang@gmail.com>
2015-10-05 17:18:23 +1300

    update env.rb file

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,6 @@
-require "Capybara"
-require "Capybara/cucumber"
+require "capybara"
+require "capybara/cucumber"
+require "rspec"
 require 'capybara/poltergeist'
 
 Capybara.default_driver = :poltergeist
